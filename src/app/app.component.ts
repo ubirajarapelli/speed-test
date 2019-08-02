0a3b3afda283a3f953e15a2914bb0b5fbcb5f644
ubirajarapelli <ubirajaracpelli@gmail.com>
2019-08-01 23:52:25 -0300

    Adiciona metodo que usuario cadastra o nome no header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Storage } from './utils/storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  constructor( private storage: Storage) { }
+
+  userName: string = 'Usuário';
+
+  userForm = new FormGroup({
+    name: new FormControl('')
+  });
+
+  ngOnInit() {
+    this.getUser();
+  };
+
+  getUser() {
+    const userStorage = this.storage.getStorage('userName');
+    if(userStorage === null) {
+      return this.userName;
+    }
+    this.userName = userStorage.name;
+  };
+
+  onSubmit() {
+    this.storage.setSorage('userName', this.userForm.value);
+    this.getUser();
+  };
+
+ }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -13,7 +14,8 @@ import { Storage } from './utils/storage';
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
